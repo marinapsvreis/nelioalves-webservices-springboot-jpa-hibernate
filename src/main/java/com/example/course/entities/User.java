54e0d88814124d6549e4c18230403cc88caaf37c
Marina Portugal <marinapsvreis@gmail.com>
2022-05-26 14:12:55 -0300

    Order, Instant, ISO8601 - PART 1

--- src/main/java/com/example/course/entities/User.java
+++ src/main/java/com/example/course/entities/User.java
@@ -1,29 +1,34 @@
 package com.example.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable{
+public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
+
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 
@@ -63,6 +68,10 @@ public class User implements Serializable{
 
 	public String getPhone() {
 		return phone;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	public void setPhone(String phone) {
