176a9f1770b18e4e06bb71ae5ed745388d207eea
Marina Portugal <marinapsvreis@gmail.com>
2022-06-05 15:18:16 -0300

    User update

--- src/main/java/com/example/course/resources/UserResource.java
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,9 +31,9 @@ public class UserResource {
 		return ResponseEntity.ok().body(list);
 	}
 	
-	@GetMapping("/{userId}")
-	public ResponseEntity<User> findById(@PathVariable Long userId){
-		User obj = userService.findById(userId);
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = userService.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 	
@@ -43,10 +44,16 @@ public class UserResource {
 		return ResponseEntity.created(uri).body(obj);
 	}
 	
-	@DeleteMapping("/{userId}")
-	public ResponseEntity<Void> delete(@PathVariable Long userId){
-		userService.delete(userId);
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		userService.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+		obj = userService.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
